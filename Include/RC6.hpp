d4e48724cc7938916620c6f46c74132cc3d3b494
Kevin <kevincheng.nrp@gmail.com>
2024-02-02 17:35:57 -0800

    Set up project

---
+++ Include/RC6.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+
+class RC6{
+private:
+
+public:
+    RC6(std::vector<uint8_t>& key);
+};
---
+++ src/RC6.cpp
@@ -0,0 +1,5 @@
+#include <RC6.hpp>
+
+RC6::RC6(std::vector<uint8_t>& key){
+    
+}
